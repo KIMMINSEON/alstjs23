b01a4f76f9038b56549a8b7e6739c5c2ac689ac0
asdf1595 <alstjs23@gmail.com>
2017-10-09 22:40:43 +0900

    Add files via upload

--- js/custom.js
+++ js/custom.js
@@ -1,5 +1,55 @@
 $(function () {
     
-   
+   // 맨위로 가기 
+   $(window).scroll(function () {
+        if ($(this).scrollTop() >= 50) {
+            $('#top').fadeIn("fast");
+        } else {
+            $('#top').fadeOut("fast");
+        }
+    });
+    $('#top').click(function () { 
+        $('body,html').animate({
+            scrollTop: 0
+        }, 1000);
+    });
+    
+    // 메뉴 스크롤
+    
+    $(window).on('scroll', function(){
+        var sT = $(window).scrollTop();
+        
+        if(sT >= 100){
+            $('header').addClass('plus',500),
+            $('header h1').addClass('plus_h'),
+            $('header nav').addClass('plus_h'),
+            $('header #trigger').addClass('plus_t'),
+            $('header span').eq(1).addClass('hidden_m');
+            
+        }else{
+            $('header').removeClass('plus',500),
+            $('header h1').removeClass('plus_h'),
+            $('header nav').removeClass('plus_h'),
+            $('header #trigger').removeClass('plus_t'),
+            $('header span').eq(1).removeClass('hidden_m');
+        }
+        
+    });
+    
+    // 모바일 메뉴
+    
+    var trigger = $('#trigger');
+    var menu = $('nav ul');
+	$(trigger).on('click', function (e) {
+        e.preventDefault();
+        $(this).toggleClass("on");
+        menu.slideToggle();
+    });
+    $(window).resize(function () {
+        var w = $(window).width();
+        if (w > 1280 && menu.is(':hidden')) {
+        menu.removeAttr('style');
+        }
+    });
     
 });
